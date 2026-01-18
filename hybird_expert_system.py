9d2b52aae61830f353787bb0dde8927c6f5227b3
Asaeyl <asaeyl202@gmail.com>
2026-01-18 13:57:03 +0300

    Update hybird_expert_system.py

--- hybird_expert_system.py
+++ hybird_expert_system.py
@@ -1625,4 +1625,6 @@ def reset_assessment():
 
 if __name__ == "__main__":
 
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT",4000))
+    app.run(host="0.0.0.0",port=port)
